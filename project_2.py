a764d81c833470174737e9e1c87dabf9facb5978
vweast7 <43427725+vweast7@users.noreply.github.com>
2018-09-20 18:06:49 -0500

    Update project_2.py
    If the reverse pattern occurred before the initial pattern wonky things happened, this should fix it

--- project_2.py
+++ project_2.py
@@ -13,7 +13,7 @@ pattern_r = pattern[::-1] #reverse pattern
 
 index_a = sequence.index(pattern)
 index_b = index_a + len(pattern)
-reverse_index = sequence.index(pattern_r)
+reverse_index = sequence.index(pattern_r, index_b)
 reverse_index_b = reverse_index + len(pattern_r)
 
 mutate_before = sequence[index_b:reverse_index]
