752ebacc2d235da5f2caa9b195faad82d070b8c5
Justin Dulay <justin.dulay@slu.edu>
2018-09-19 21:33:33 -0500

    this is cs_1300 project 2

---
+++ project_2.py
@@ -0,0 +1,25 @@
+#Justin Dulay
+#DNA Mutator
+
+sequence = input('Enter a DNA sequence: ')
+pattern = input('Enter the pattern: ')
+
+if pattern in sequence == False:
+  print(pattern+' is not in '+sequence+'\n')
+  pattern = input('Enter the pattern: ')
+
+pattern_r = pattern[::-1] #reverse pattern
+
+
+index_a = sequence.index(pattern)
+index_b = index_a + len(pattern)
+reverse_index = sequence.index(pattern_r)
+reverse_index_b = reverse_index + len(pattern_r)
+
+mutate_before = sequence[index_b:reverse_index]
+mutate = mutate_before[::-1]
+statement = 'Mutate DNA sequence: '
+
+print(statement + sequence[:index_a] + pattern + mutate + pattern_r + sequence[reverse_index_b:])
+
+ 
