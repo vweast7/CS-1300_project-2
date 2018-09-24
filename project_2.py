aaedf2b645217e71c148dec2956ed37e599ef3ca
vweast7 <43427725+vweast7@users.noreply.github.com>
2018-09-24 17:51:23 -0500

    Update project_2.py

--- project_2.py
+++ project_2.py
@@ -4,21 +4,21 @@
 sequence = input('Enter a DNA sequence: ')
 pattern = input('Enter the pattern: ')
 
-if pattern in sequence == False:
-  print(pattern+' is not in '+sequence+'\n')
-  pattern = input('Enter the pattern: ')
+if pattern not in sequence:
+  print(pattern, 'not in DNA sequence.')
+  pattern = input('Enter a new pattern: ')
 
 pattern_r = pattern[::-1] #reverse pattern
 
 
 index_a = sequence.index(pattern)
 index_b = index_a + len(pattern)
-reverse_index = sequence.index(pattern_r)
+reverse_index = sequence.index(pattern_r, index_b)
 reverse_index_b = reverse_index + len(pattern_r)
 
 mutate_before = sequence[index_b:reverse_index]
 mutate = mutate_before[::-1]
-statement = 'Mutate DNA sequence: '
+statement = 'Mutated DNA sequence: '
 
 print(statement + sequence[:index_a] + pattern + mutate + pattern_r + sequence[reverse_index_b:])
 
